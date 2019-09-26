e394e7348dcc6ee7eed80edd802c35279097dad7
Joshua Williamson <jlwilliamson95@gmail.com>
2019-09-26 12:46:22 -0600

    Added needed debugging output

--- WPFCommandPanel/GenReportEvents.cs
+++ WPFCommandPanel/GenReportEvents.cs
@@ -37,98 +37,134 @@ namespace WPFCommandPanel
         }
         private void CreateReport(object sender, DoWorkEventArgs e)
         {
-            var s = new System.Diagnostics.Stopwatch();
-            s.Start();
-            this.Dispatcher.Invoke(() =>
+            try
             {
-                Run run = new Run($"Generating Report\n")
-                {
-                    Foreground = System.Windows.Media.Brushes.Cyan
-                };
-                TerminalOutput.Inlines.Add(run);
-            });
 
-            var checkedDomain = this.Dispatcher.Invoke(() =>
-            {
-                var domain = RadioButtonGroup?.Children?.OfType<RadioButton>()
-                ?.FirstOrDefault(r => r.IsChecked == true)
-                ?.Content
-                ?.ToString();
-                return domain;
-            });
-            
-            if (checkedDomain == null)
-            {
+
+                var s = new System.Diagnostics.Stopwatch();
+                s.Start();
                 this.Dispatcher.Invoke(() =>
                 {
-                    MessageBox.Show("No domain chosen.");
+                    Run run = new Run($"Generating Report\n")
+                    {
+                        Foreground = System.Windows.Media.Brushes.Cyan
+                    };
+                    TerminalOutput.Inlines.Add(run);
                 });
-                return;
-            }
-            var text = this.Dispatcher.Invoke(() =>
-            {
-                return CourseID.Text;
-            });
-            if (text == null)
-            {
-                this.Dispatcher.Invoke(() =>
+
+                var checkedDomain = this.Dispatcher.Invoke(() =>
                 {
-                    MessageBox.Show("No course ID found.");
+                    var domain = RadioButtonGroup?.Children?.OfType<RadioButton>()
+                    ?.FirstOrDefault(r => r.IsChecked == true)
+                    ?.Content
+                    ?.ToString();
+                    return domain;
                 });
-                return;
-            }
-            CanvasApi.ChangeDomain(checkedDomain);
 
-            CourseInfo course;
-            bool directory = false;
-            LinkParser ParseForLinks = null; //Need to declare this early as it is only set if it is a directory
-            if (int.TryParse(text, out int id))
-            {
-                //It is an ID, create course info with new id var
-                course = new CourseInfo(id);
-            }
-            else
-            {
-                //Just send it in as a string path
-                course = new CourseInfo(text);
-                directory = true;
-                ParseForLinks = new LinkParser(course.CourseIdOrPath);
-            }
-
-            if(course == null || course.CourseCode == null)
-            {
-                e.Result = "Could not find course. ID was entered wrong or canvas needs time to cool down ...\n";
-                return;
-            }
-            A11yParser ParseForA11y = new A11yParser();
-            MediaParser ParseForMedia = new MediaParser();
-
-            Parallel.ForEach(course.PageHtmlList, page =>
-            {
-                ParseForA11y.ProcessContent(page);
-                ParseForMedia.ProcessContent(page);
-                if (directory)
+                if (checkedDomain == null)
                 {
-                    ParseForLinks.ProcessContent(page);
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        MessageBox.Show("No domain chosen.");
+                    });
+                    return;
                 }
-            });
-           
-            var file_name_extention = ((CanvasApi.CurrentDomain == "Directory") ? System.IO.Path.GetPathRoot(text) + "Drive" : CanvasApi.CurrentDomain).Replace(":\\", "");
-            CreateExcelReport GenReport = new CreateExcelReport(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + $"\\AccessibilityTools\\ReportGenerators-master\\Reports\\ARC_{course.CourseCode.Replace(",", "").Replace(":", "")}_{file_name_extention}.xlsx");
-            GenReport.CreateReport(ParseForA11y.Data, ParseForMedia.Data, ParseForLinks?.Data);
-            s.Stop();
-            ParseForMedia.Chrome.Quit();
-            if (ParseForA11y.Data.Count() > int.Parse(File.ReadAllText(@"M:\DESIGNER\Content EditorsELTA\Accessibility Assistants\HIGHSCORE.txt")))
-            {
-                File.WriteAllText(@"M:\DESIGNER\Content EditorsELTA\Accessibility Assistants\HIGHSCORE.txt", ParseForA11y.Data.Count().ToString());
-                Dispatcher.Invoke(() =>
+                var text = this.Dispatcher.Invoke(() =>
                 {
-                    HighScoreBox.Text = "HighScore: " + File.ReadAllText(@"M:\DESIGNER\Content EditorsELTA\Accessibility Assistants\HIGHSCORE.txt");
+                    return CourseID.Text;
                 });
+                if (text == null)
+                {
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        MessageBox.Show("No course ID found.");
+                    });
+                    return;
+                }
+                CanvasApi.ChangeDomain(checkedDomain);
+                this.Dispatcher.Invoke(() =>
+                {
+                    Run run = new Run($"Domain changed to {checkedDomain}\n")
+                    {
+                        Foreground = System.Windows.Media.Brushes.Green
+                    };
+                    TerminalOutput.Inlines.Add(run);
+                });
+
+                CourseInfo course;
+                bool directory = false;
+                LinkParser ParseForLinks = null; //Need to declare this early as it is only set if it is a directory
+                if (int.TryParse(text, out int id))
+                {
+                    //It is an ID, create course info with new id var
+                    course = new CourseInfo(id);
+                }
+                else
+                {
+                    //Just send it in as a string path
+                    course = new CourseInfo(text);
+                    directory = true;
+                    ParseForLinks = new LinkParser(course.CourseIdOrPath);
+                }
+                this.Dispatcher.Invoke(() =>
+                {
+                    Run run = new Run($"Created course object\n")
+                    {
+                        Foreground = System.Windows.Media.Brushes.Green
+                    };
+                    TerminalOutput.Inlines.Add(run);
+                });
+                if (course == null || course.CourseCode == null)
+                {
+                    e.Result = "Could not find course. ID was entered wrong or canvas needs time to cool down ...\n";
+                    return;
+                }
+                this.Dispatcher.Invoke(() =>
+                {
+                    Run run = new Run($"Beginning to parse pages\n")
+                    {
+                        Foreground = System.Windows.Media.Brushes.Green
+                    };
+                    TerminalOutput.Inlines.Add(run);
+                });
+                A11yParser ParseForA11y = new A11yParser();
+                MediaParser ParseForMedia = new MediaParser();
+                Parallel.ForEach(course.PageHtmlList, page =>
+                {
+                    ParseForA11y.ProcessContent(page);
+                    ParseForMedia.ProcessContent(page);
+                    if (directory)
+                    {
+                        ParseForLinks.ProcessContent(page);
+                    }
+                });
+                this.Dispatcher.Invoke(() =>
+                {
+                    Run run = new Run($"Finished parsing pages, creating file\n")
+                    {
+                        Foreground = System.Windows.Media.Brushes.Green
+                    };
+                    TerminalOutput.Inlines.Add(run);
+                });
+                var file_name_extention = ((CanvasApi.CurrentDomain == "Directory") ? System.IO.Path.GetPathRoot(text) + "Drive" : CanvasApi.CurrentDomain).Replace(":\\", "");
+                CreateExcelReport GenReport = new CreateExcelReport(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + $"\\AccessibilityTools\\ReportGenerators-master\\Reports\\ARC_{course.CourseCode.Replace(",", "").Replace(":", "")}_{file_name_extention}.xlsx");
+                GenReport.CreateReport(ParseForA11y.Data, ParseForMedia.Data, ParseForLinks?.Data);
+                s.Stop();
+                ParseForMedia.Chrome.Quit();
+                if (ParseForA11y.Data.Count() > int.Parse(File.ReadAllText(@"M:\DESIGNER\Content EditorsELTA\Accessibility Assistants\HIGHSCORE.txt")))
+                {
+                    File.WriteAllText(@"M:\DESIGNER\Content EditorsELTA\Accessibility Assistants\HIGHSCORE.txt", ParseForA11y.Data.Count().ToString());
+                    Dispatcher.Invoke(() =>
+                    {
+                        HighScoreBox.Text = "HighScore: " + File.ReadAllText(@"M:\DESIGNER\Content EditorsELTA\Accessibility Assistants\HIGHSCORE.txt");
+                    });
+                }
+
+                e.Result = $"Report generated.\nTime taken: {s.Elapsed.ToString(@"hh\:mm\:ss")}\n";
+            }catch(Exception ex)
+            {
+                e.Result = ex.Message + '\n' + ex.ToString() + '\n' + ex.StackTrace;
             }
-
-            e.Result = $"Report generated.\nTime taken: {s.Elapsed.ToString(@"hh\:mm\:ss")}\n";
-
         }
         private void ReviewPage_Click(object sender, EventArgs e)
         {
